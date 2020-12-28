e946fc7dc0274e89269cb282952aa53ec895a8f8
gerardayissi <49698143+gerardayissi@users.noreply.github.com>
2020-12-28 02:08:18 -0600

    add env variable

--- gcs-to-pubsub/gcs-item-pubsub/gcs-to-pubsub-run-job.sh
+++ gcs-to-pubsub/gcs-item-pubsub/gcs-to-pubsub-run-job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-gcloud dataflow jobs run gerard-gcs-to-pubsub-sample-001 \
+export GCLOUD_PATH=/Users/hgo2/Downloads/google-cloud-sdk/bin
+
+${GCLOUD_PATH}/gcloud dataflow jobs run gerard-gcs-to-pubsub-sample-001 \
 --region=us-east4  \
 --gcs-location gs://tst1-integration-3ca6-gerard/pipeline/template/template-gcs-sample-001
