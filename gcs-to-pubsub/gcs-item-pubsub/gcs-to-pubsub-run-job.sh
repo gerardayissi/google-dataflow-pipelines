1441f6f25f9e9f9564e75adabf2695c7e6e0de09
gerardayissi <49698143+gerardayissi@users.noreply.github.com>
2020-12-28 01:17:19 -0600

    fix local directory 05

--- gcs-to-pubsub/gcs-item-pubsub/gcs-to-pubsub-run-job.sh
+++ gcs-to-pubsub/gcs-item-pubsub/gcs-to-pubsub-run-job.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 gcloud dataflow jobs run gerard-gcs-to-pubsub-sample-001 \
 --region=us-east4  \
 --gcs-location gs://tst1-integration-3ca6-gerard/pipeline/template/template-gcs-sample-001
